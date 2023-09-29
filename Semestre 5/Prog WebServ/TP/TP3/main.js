eb84a3d62b8ad81daf08e5d8d0910949dd53e46a
NikoGandon <nikola.gandon@gmail.com>
2023-09-29 17:53:13 +0200

    TP 3

--- Semestre 5/Prog WebServ/TP/TP3/main.js
+++ Semestre 5/Prog WebServ/TP/TP3/main.js
@@ -1,11 +1,85 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParse = require('body-parser')
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+	  // Spécifiez le dossier de destination où les fichiers seront stockés
+	  callback(null, 'uploads/');
+	},
+	filename: (req, file, callback) => {
+	  // Spécifiez le nom du fichier une fois qu'il est téléchargé
+	  callback(null, Date.now() + '-' + file.originalname);
+	},
+  });
+
+const upload = multer(storage);
+
+app.use(bodyParse.json())
 
 app.get('/', (req, res) => {res.send("HW")})
 app.listen(port, () => {
 	console.log("server listening @ port")
 })
 app.get('/Hey', (req, res) => {res.send("Hey")})
-app.get('/Calcul', (req, res) => {res.send("Un calculateur un matheux")})
-app.get('/Calculatrice', (req, res) => {res.send("C'est une pute")})
+app.get('/Calcul/:num1/:num2/:operateur', (req, res) => {
+	
+	const num1 = parseInt(req.params.num1);
+	const num2 = parseInt(req.params.num2);
+	const operateur = req.params.operateur;
+	let result = 0;
+
+	if (operateur === "+") {
+		result = num1 + num2;
+	} else if (operateur === "-") {
+		result = num1 - num2;
+	} else if (operateur === "*") {
+		result = num1 * num2;
+	} else if (operateur === "/") {
+		if (num2 === 0) {
+			// Gérer la division par zéro ici si nécessaire
+			res.status(400).send("Division par zéro impossible");
+			return;
+		}
+		result = num1 / num2;
+	} else {
+		// Gérer un opérateur non pris en charge ici si nécessaire
+		res.status(400).send("Opérateur non pris en charge");
+		return;
+	}
+
+	res.send("Résultat = " + result);
+});
+
+app.post('/constante', (req, res) => {
+	const {name, value} = req.body;
+
+	const constante = {}
+	if (constante[name]){
+		res.status(400).send("La constante existe déjà");
+	}	
+	else  {
+		constante[name] = value;
+		res.status(200).send("La constante a bien été ajoutée " + constante[name]);
+	}
+});
+
+app.post('/profil', (req, res) => {
+		//Créer une variable ayant pour valeur test  Prend en paramètres une chaine de caractères qu’il faudra concatèner à
+		//la variable créée précédemment. Renvoie le résultat
+
+		let maVar = "test";
+		function concatener(parametre) {
+			maVar = maVar + parametre;
+			return maVar;
+		}
+		const resultat = concatener(req.params.profil);
+		res.send(resultat);
+		
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+		res.send('Fichier téléchargé avec succès !');
+});
