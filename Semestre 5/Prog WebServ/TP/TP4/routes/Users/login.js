c8d2a54041883f39b0a35a035105abb5302d5e82
NikoGandon <nikola.gandon@gmail.com>
2023-10-08 00:49:11 +0200

    feat: login utilisateur

---
+++ Semestre 5/Prog WebServ/TP/TP4/routes/Users/login.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken');
+const { comparePassword } = require('./AuthHelper');
+const passport = require('./passportRoute');
+
+
+const secretKey = process.env.ACCESS_TOKEN;
+
+function createToken(user){
+	return jwt.sign({ 
+		id : user.id,
+		username: user.username }, secretKey, { expiresIn: '1h' });
+}
+
+app.post('/login', passport.authenticate('local', {session : false}) ,(req, res, next) => {
+	const user = req.user;
+	const token = createToken(user);
+	res.json({ token });
+})
+
+module.exports = app;
