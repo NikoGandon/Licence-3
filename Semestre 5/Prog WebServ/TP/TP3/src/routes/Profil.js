8bfbad6e73946144adda7c6ba1a8ae9d6585561b
Nikola <nikola.gandon@gmail.com>
2023-10-20 22:23:34 +0200

    PWS : TP 3/ Correction verbe HTTP

--- Semestre 5/Prog WebServ/TP/TP3/src/routes/Profil.js
+++ Semestre 5/Prog WebServ/TP/TP3/src/routes/Profil.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/profil", (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post("/profil", (req, res) => {
     let maVar = "test";
     function concatener(parametre) {
       maVar = maVar + parametre;
       return maVar;
     }
-    const resultat = concatener(req.params.profil);
-    res.send(resultat);
+    const resultat = concatener(req.body.profil);
+    res.send("resultat de la concaténation : " + resultat);
 });
 
 module.exports = app;
