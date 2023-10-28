b4f5a0bc4bf8346ff803f3604933061bc6c3a63f
NikoGandon <nikola.gandon@gmail.com>
2023-10-28 22:17:20 +0200

    PWS : Redirect from signup To login

--- Semestre 5/Prog WebServ/TP/TP4/routes/Users/register.js
+++ Semestre 5/Prog WebServ/TP/TP4/routes/Users/register.js
@@ -1,53 +1,40 @@
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
 const db = require("../config");
 const { hashPassword } = require("./AuthHelper");
 
-const secretKey = process.env.ACCESS_TOKEN;
-
-function createToken(user) {
-  return jwt.sign(
-    {
-      id: user.id,
-      username: user.username,
-      admin: user.isAdmin ? true : false,
-    },
-    secretKey,
-    { expiresIn: process.env.DURATION_TOKEN }
-  );
-}
-
 app.post("/register", (req, res, next) => {
   const { username, password } = req.body;
-  db.query(
-    "SELECT * FROM utilisateurs WHERE username = ?",
-    [username],
-    (err, result) => {
-      if (err) {
-        return next(err);
-      }
-
-      if (result.length > 0) {
-        return res.status(401).json({ message: "Utilisateur deja existant" });
-      }
-
-      const hash = hashPassword(password);
-
-      db.query(
-        "INSERT INTO utilisateurs (username, password) VALUES (?, ?)",
-        [username, hash],
-        (err, result) => {
-          if (err) {
-            return next(err);
-          }
-
-          const token = createToken({ id: result.insertId, username });
-          return res.json({ token });
+  try {
+    db.query(
+      "SELECT * FROM utilisateurs WHERE username = ?",
+      [username],
+      (err, result) => {
+        if (err) {
+          return next(err);
         }
-      );
-    }
-  );
+
+        if (result.length > 0) {
+          return res.status(401).json({ message: "Utilisateur deja existant" });
+        }
+
+        const hash = hashPassword(password);
+
+        db.query(
+          "INSERT INTO utilisateurs (username, password) VALUES (?, ?)",
+          [username, hash],
+          (err, result) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(307, "/login");
+          }
+        );
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ message: "Signup failed" });
+  }
 });
 
 module.exports = app;
