98d599a97c9b72faec732c92ac7ea158d662bebf
NikoGandon <nikola.gandon@gmail.com>
2023-10-29 02:05:44 +0100

    PWS : TP 4/ AuthToken Message d'erreur pour les non-connectés ou ceux qui ne doivent pas avoir d'accès au service

--- Semestre 5/Prog WebServ/TP/TP4/middleware/AuthToken.js
+++ Semestre 5/Prog WebServ/TP/TP4/middleware/AuthToken.js
@@ -6,7 +6,9 @@ function verifyToken(req, res, next) {
   const token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Accès non autorisé aux non-connectés." });
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé aux non-connectés." });
   }
 
   try {
@@ -14,26 +16,38 @@ function verifyToken(req, res, next) {
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Accès non autorisé : identification impossible." });
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé : identification impossible." });
   }
 }
 
 function verifyAdminToken(req, res, next) {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : null;
 
   if (!token) {
-    return res.status(401).json({ message: "Accès non autorisé aux non-connectés." });
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé aux non-connectés." });
   }
-
   try {
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded;
     if (!req.user.admin) {
-      return res.status(401).json({ message: "Accès non autorisé." });
+      return res
+        .status(401)
+        .json({
+          message:
+            "Accès non autorisé. Vous ne possédez pas les droits pour accéder à ce service",
+        });
     }
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Accès non autorisé : identification impossible." });
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé : identification impossible." });
   }
 }
 
