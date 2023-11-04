3a4f778e3b2429f1d31c9759b16f1177d61256e3
NikoGandon <nikola.gandon@gmail.com>
2023-11-04 04:01:45 +0100

    PWS : TP 5/ ajout de routes

--- Semestre 5/Prog WebServ/TP/TP5/src/main.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/main.js
@@ -1,81 +1,32 @@
-require('dotenv').config();
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const bodyParser = require("body-parser"); 
 
-
-const storageRouter = require('./routes/storage');
-const registerRouter = require('./routes/Users/register');
-const loginRouter = require('./routes/Users/login');
-const deleteRouter = require('./routes/delete');
-
-const checkToken = require('./middleware/AuthToken');
-
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const Hey = require("./routes/Hey");
+const Profil = require("./routes/Profil");
+const Calcul = require("./routes/Calcul");
+const Constante = require("./routes/Constante");
+const Storage = require("./routes/storage");
+const Register = require("./routes/Users/register");
+const Login = require("./routes/Users/login");
+const Delete = require("./routes/Delete");
 
-app.get('/', (req, res) => {res.send("HW")})
+const checkToken = require("./middleware/AuthToken");
+
 app.listen(port, () => {
-	console.log("server listening @ port")
-})
-
-
-app.get('/Hey', (req, res) => {res.send("Hey")})
-app.get('/Calcul/:num1/:num2/:operateur', (req, res) => {
-	
-	const num1 = parseInt(req.params.num1);
-	const num2 = parseInt(req.params.num2);
-	const operateur = req.params.operateur;
-	let result = 0;
-
-	if (operateur === "+") {
-		result = num1 + num2;
-	} else if (operateur === "-") {
-		result = num1 - num2;
-	} else if (operateur === "*") {
-		result = num1 * num2;
-	} else if (operateur === "/") {
-		if (num2 === 0) {
-			res.status(400).send("Division par zéro impossible");
-			return;
-		}
-		result = num1 / num2;
-	} else {
-		res.status(400).send("Opérateur non pris en charge");
-		return;
-	}
-
-	res.send("Résultat = " + result);
+  console.log("server listening @ port " + port);
 });
 
-app.post('/constante', (req, res) => {
-	const {name, value} = req.body;
-
-	const constante = {}
-	if (constante[name]){
-		res.status(400).send("La constante existe déjà");
-	}	
-	else  {
-		constante[name] = value;
-		res.status(200).send("La constante a bien été ajoutée " + constante[name]);
-	}
-});
-
-app.post('/profil', checkToken.verifyToken,(req, res) => {
-
-		let maVar = "test";
-		function concatener(parametre) {
-			maVar = maVar + parametre;
-			return maVar;
-		}
-		const resultat = concatener(req.params.profil);
-		res.send(resultat);
-		
-});
-
-app.post('/login', loginRouter);
-app.post('/register', registerRouter);
-app.post('/upload', checkToken.verifyAdminToken, storageRouter);
-app.delete('/delete', checkToken.verifyAdminToken, deleteRouter);
+app.get("/Hey", Hey);
+app.get("/Calcul/:num1/:num2/:operateur", checkToken.verifyToken, Calcul);
+app.post("/constante", checkToken.verifyToken, Constante);
+app.post("/profil", Profil);
+app.post("/login", Login);
+app.post("/register", Register);
+app.post("/upload", checkToken.verifyAdminToken, Storage);
+app.delete("/delete", checkToken.verifyAdminToken, Delete);
