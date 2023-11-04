c13899f0fc07826687d8538e736e053fd71f0359
NikoGandon <nikola.gandon@gmail.com>
2023-11-04 22:39:52 +0100

    PWS : TP 5/ Utilisation de Sequelize

--- Semestre 5/Prog WebServ/TP/TP5/src/middleware/passportLogin.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/middleware/passportLogin.js
@@ -3,59 +3,46 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const db = require("../routes/config");
 const { comparePassword } = require("../routes/Users/AuthHelper");
+const User = require("../Models/User.model");
 
-passport.use('localLogin',
-  new LocalStrategy((username, password, done) => {
-    db.query(
-      "SELECT * FROM utilisateurs WHERE username = ?",
-      [username],
-      (err, result) => {
-        if (err) {
-          return done(err);
-        }
-        if (!result || result.length === 0) {
-          return done(null, false, { message: "Utilisateur inexistant" });
-        }
+passport.use('localLogin', new LocalStrategy(async (username, password, done) => {
+  try {
+    const userExisting = await User.findOne({ where: { username: username } });
+    if (!userExisting) {
+      return done(null, false, { message: "Utilisateur inexistant" });
+    }
+    const user = userExisting.dataValues;
+    if (!comparePassword(password, user.password)) {
+      return done(null, false, { message: "Mot de passe incorrect" });
+    }
+    const admin = await db.query('SELECT * FROM admin WHERE id = :id', {
+      replacements: { id: user.id },
+      type: db.QueryTypes.SELECT
+    });
+    if (admin.length > 0) {
+      user.isAdmin = true;
+    } else {
+      user.isAdmin = false;
+    }
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
 
-        const user = result[0];
+}));
 
-        if (!comparePassword(password, user.password)) {
-          return done(null, false, { message: "Mot de passe incorrect" });
-        }
-
-        db.query(
-          "SELECT * FROM admin WHERE id = ?",
-          [user.id],
-          (adminErr, result) => {
-            if (adminErr) {
-              return done(adminErr);
-            }
-            if (result.length > 0) {
-              user.isAdmin = true;
-            } else {
-              user.isAdmin = false;
-            }
-            return done(null, user);
-          }
-        );
-      }
-    );
-  })
-);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  db.query("SELECT * FROM utilisateurs WHERE id = ?", [id], (err, result) => {
-    if (err) {
-      return done(err);
-    }
-
-    const user = result[0];
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findOne({ where: { id: id } });
     return done(null, user);
-  });
+  } catch (error) {
+    return done(error);
+  }
 });
 
 module.exports = passport;
