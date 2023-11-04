ad4a94a9b86dae9254a987a58e3b4585a52867b6
NikoGandon <nikola.gandon@gmail.com>
2023-11-04 03:45:08 +0100

    PWS : TP 5/ Suppression lignes inutiles

--- Semestre 5/Prog WebServ/TP/TP5/src/routes/config.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/routes/config.js
@@ -1,31 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mysql = require('mysql2');
-const {Sequelize, DataType} = require('sequelize');
-/*
-const db = mysql.createConnection({
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
-});
+    dialect: "mysql",
+  }
+);
 
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connecté à la base MySQL');
-});*/
-
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-});
-
-db.authenticate().then(() => {
-    console.log('Connecté à la base MySQL');
-}).catch(err => {
-    console.error('Impossible de se connecter à la base de données:', err);
-});
+db.authenticate()
+  .then(() => {
+    console.log("Connecté à la base MySQL");
+  })
+  .catch((err) => {
+    console.error("Impossible de se connecter à la base de données:", err);
+  });
 
 module.exports = db;
