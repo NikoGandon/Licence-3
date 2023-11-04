ac8750de5d070a16d7354f7453c57bb29de888f8
NikoGandon <nikola.gandon@gmail.com>
2023-11-04 03:36:15 +0100

    PWS : TP 4/ Ajout des routes du TP 3

---
+++ Semestre 5/Prog WebServ/TP/TP4/src/routes/Constante.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+let constante = {};
+
+app.post("/constante", (req, res) => {
+    const { name, value } = req.body;
+  
+    if (constante[name]) {
+      res.status(400).send("La constante existe déjà");
+    } else {
+      constante[name] = value;
+      res
+        .status(200)
+        .send(
+          "La constante " +
+            name +
+            " a bien été ajoutée avec la valeur " +
+            constante[name]
+        );
+    }
+  });
+
+module.exports = app;
