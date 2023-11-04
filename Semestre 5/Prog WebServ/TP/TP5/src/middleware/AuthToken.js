ef4a296704716839ecb970f5e401e6645801d680
NikoGandon <nikola.gandon@gmail.com>
2023-11-04 04:02:59 +0100

    PWS : TP 5/ mise en forme + gestion non-connecté

--- Semestre 5/Prog WebServ/TP/TP5/src/middleware/AuthToken.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/middleware/AuthToken.js
@@ -1,42 +1,69 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+
 const secretKey = process.env.ACCESS_TOKEN;
 
-function verifyToken(req, res, next){
-    const token = req.headers.authorization.split(" ")[1];
-
-    if (!token){
-        return res.status(401).json({message: "Token inexistant"});
-    }
-
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        req.user = decoded;
-        next();
-    }
-    catch (err) {
-        return res.status(401).json({message: "Token invalide"});
-    }
+function createToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      username: user.username,
+      admin: user.isAdmin ? true : false,
+    },
+    secretKey,
+    { expiresIn: process.env.DURATION_TOKEN }
+  );
 }
 
-function verifyAdminToken(req, res, next){
-    const token = req.headers.authorization.split(" ")[1];
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : null;
+    
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé aux non-connectés." });
+  }
 
-    if (!token){
-        return res.status(401).json({message: "Token inexistant"});
-    }
-
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        req.user = decoded;
-        if (!req.user.admin){
-            return res.status(401).json({message: "Token invalide"});
-        }
-        next();
-    }
-    catch (err) {
-        return res.status(401).json({message: "Token invalide"});
-    }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé : identification impossible." });
+  }
 }
 
-module.exports = { verifyToken, verifyAdminToken };
+function verifyAdminToken(req, res, next) {
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : null;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Accès non autorisé aux non-connectés." });
+  }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+    if (!req.user.admin) {
+      return res.status(401).json({
+        message:
+          "Accès non autorisé. Vous ne possédez pas les droits pour accéder à ce service",
+      });
+    }
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({
+        message: "Accès non autorisé : identification impossible : " + err,
+      });
+  }
+}
+
+module.exports = { createToken, verifyToken, verifyAdminToken };
