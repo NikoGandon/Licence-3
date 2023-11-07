1994e37c156d2c0d7a40ba3149e8b499dce2b73b
NikoGandon <nikola.gandon@gmail.com>
2023-11-07 02:44:22 +0100

    PWS : TP 5/ Affichage des erreurs de connexion et d'inscription

--- Semestre 5/Prog WebServ/TP/TP5/src/routes/Users/register.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/routes/Users/register.js
@@ -3,17 +3,26 @@ const app = express();
 const passport = require("../../middleware/passportRegister");
 
 app.post("/register", (req, res, next) => {
-  passport.authenticate("localRegister", (err, user, info) => {
-    if (err) {
-      console.log(err);
-      return next(err);
-    }
-    if (!user) {
-      console.log(info);
-      return res.status(401).json({ message: info.message });
-    }
-    res.redirect(307, "/login");
-  })(req, res, next);
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: "Tous les champs ne sont pas remplis." });
+  }
+  try {
+    passport.authenticate("localRegister", (err, user, info) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+  
+      if (!user) {
+        return res.status(401).json({ message: info.message });
+      }
+  
+      res.redirect(307, "/login");
+  
+    })(req, res, next);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = app;
