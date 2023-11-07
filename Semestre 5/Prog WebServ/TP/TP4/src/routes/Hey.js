ef80ee59b798128004aa7c70acd42302af7d1406
NikoGandon <nikola.gandon@gmail.com>
2023-11-07 02:43:14 +0100

    PWS : TP 4/ Affichage du TP sur la route Hey

--- Semestre 5/Prog WebServ/TP/TP4/src/routes/Hey.js
+++ Semestre 5/Prog WebServ/TP/TP4/src/routes/Hey.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/Hey", (req, res) => {
-    res.send("Hey");
+    res.send("Hey, ceci est le TP 4");
 });
 
 module.exports = app;
