6cca1d408a56045eee09fe5663b0cda164c7142f
NikoGandon <nikola.gandon@gmail.com>
2023-11-24 19:32:28 +0100

    PWS : TP 5/ Ajout model admin

--- Semestre 5/Prog WebServ/TP/TP5/src/middleware/passportLogin.js
+++ Semestre 5/Prog WebServ/TP/TP5/src/middleware/passportLogin.js
@@ -1,9 +1,9 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-const db = require("../routes/config");
 const { comparePassword } = require("../routes/Users/AuthHelper");
 const User = require("../Models/User.model");
+const Admin = require("../Models/Admin.model");
 
 passport.use(
   "localLogin",
@@ -12,21 +12,22 @@ passport.use(
       const userExisting = await User.findOne({
         where: { username: username },
       });
+
       if (!userExisting) {
         return done(null, false, { message: "Utilisateur inexistant" });
       }
+
       const user = userExisting.dataValues;
       if (!comparePassword(password, user.password)) {
         return done(null, false, { message: "Mot de passe incorrect" });
       }
-      const admin = await db.query("SELECT * FROM admin WHERE id = :id", {
-        replacements: { id: user.id },
-        type: db.QueryTypes.SELECT,
-      });
+
+      const admin = await Admin.findOne({ where: { id: user.id } });
       user.isAdmin = false;
-      if (admin.length > 0) {
+      if (admin) {
         user.isAdmin = true;
       }
+
       return done(null, user);
     } catch (error) {
       return done(error);
