5f083b8c0e1456335e51fb5a328e9382d36925ac
NikoGandon <nikola.gandon@gmail.com>
2024-01-16 11:19:06 +0100

    AND: TD3

---
+++ Semestre 6/prog mob/TD3/app/src/main/java/com/example/td3/ui/layout2.java
@@ -0,0 +1,28 @@
+package com.example.td3.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.td3.R;
+
+
+public class layout2 extends  AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(layout2.this, layout1.class));
+            }
+        });
+    }
+}
