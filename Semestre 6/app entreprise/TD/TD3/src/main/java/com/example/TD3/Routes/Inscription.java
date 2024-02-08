3c709c898e3ada6906da93d04ce971beea98c09f
NikoGandon <nikola.gandon@gmail.com>
2024-02-08 15:28:39 +0100

    ENT: avancement TD3

--- Semestre 6/app entreprise/TD/TD3/src/main/java/com/example/TD3/Routes/Inscription.java
+++ Semestre 6/app entreprise/TD/TD3/src/main/java/com/example/TD3/Routes/Inscription.java
@@ -1,5 +1,6 @@
 package com.example.TD3.Routes;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,30 +9,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionAttributeListener;
 
 import org.springframework.ui.Model;
 
 
 @Controller
-public class Inscription {
+@Component
+public class Inscription extends HttpFilter implements HttpSessionAttributeListener{
     @GetMapping("/inscription")
     public String getInscription() {
         return "formulaireInscription";
     }
 
     @PostMapping("/inscription")
+    @ResponseBody
     public String postInscription(@RequestBody String prenom, HttpServletRequest httpServletRequest) {
         
         HttpSession session = httpServletRequest.getSession();
 
+        String prenomUtilisateur = prenom;
+
         System.out.println("------------------");
-        System.out.println(prenom);
+        System.out.println(prenomUtilisateur);
         System.out.println("------------------");
 
-        session.setAttribute("prenomUtilisateur", prenom);
+        session.setAttribute("prenomUtilisateur", prenomUtilisateur);
 
-        return "confirmation";
+        return "confirmation " + prenomUtilisateur;
     }
+
+    @Override
+    protected void doFilter(HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain chain) {
+        return;
+    }
+
 }
